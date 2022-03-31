a407f11dad0574647f58e97592f04b8582d157fb
Slawomir Koszewski <slawomir.koszewski@medicover.com>
2022-03-31 08:36:51 +0000

    Removed executable status from reset-env.sh. It must be sourced to be effective.

--- reset-env.sh
+++ reset-env.sh
@@ -1,15 +1,22 @@
-#!/usr/bin/env bash
+# NOTICE: This script MUST be executed using source mechanism:
+#
+# . reset-env.sh
+#
+# It cannot be executed in a separate process!
+#
 
 if [ -z "$1" ]
 then
     echo "ERROR: Argument missing. Specify environment file name."
-    exit 1
+    return 1
 fi
 
 if [ ! -f "$1" ]
 then
     echo "ERROR: \"$1\" does not exit."
-    exit 1
+    return 1
 fi
 
 unset $(cat "$1" | awk '/^export/ { print $2 }' | cut -d= -f1)
+
+return 0
