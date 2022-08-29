a50c7b32262c0fac0548e71bd7827117effcceb0
Slawomir Koszewski <slawomir.koszewski@medicover.com>
2022-08-29 12:51:23 +0200

    Added options to show-ca script.

--- show-ca.sh
+++ show-ca.sh
@@ -2,10 +2,32 @@
 
 . functions.sh
 
+usage() {
+    echo "Usage: $0 { -u | -c }"
+}
+
 # Check, if the environment has been sourced. Stop, if not.
-check_env || exit 1
+check_env -v || exit 1
 
-echo "Showing CA certificate from \"$CA_ROOT\":"
-echo ""
+while getopts "uc" option
+do
+    case $option in
+        c)
+            echo "Showing CA certificate from \"$CA_ROOT\":"
+            echo ""
 
-openssl x509 -noout -text -nameopt multiline -certopt no_pubkey,no_sigdump -in "$CA_ROOT/$CA_NAME.crt"
+            openssl x509 -noout -text -nameopt multiline -certopt no_pubkey,no_sigdump -in "$CA_ROOT/$CA_NAME.crt"
+            ;;
+        u)
+            echo "The server is available at \"$SERVER_WWW_PROTOCOL://$SERVER_FQDN\"."
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
