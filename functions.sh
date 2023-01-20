45dfed0f9dd032c71830b0b98544a4489e7e0df5
Slawomir Koszewski <slawek@koszewscy.waw.pl>
2023-01-20 23:55:21 +0100

    Fixed incorrect protocol designation for TCP client.

--- functions.sh
+++ functions.sh
@@ -172,7 +172,7 @@ show_profile() {
 setenv PROFILE_NAME $BASE_NAME
 client
 dev tun
-proto ${SERVER_PROTOCOL/tcp/tcp-server}
+proto $SERVER_PROTOCOL
 remote $SERVER_FQDN $SERVER_PORT
 data-ciphers AES-256-GCM:AES-256-CBC
 auth SHA256
